fcd0839c8ac294c830ade90a9cf523423bfc7604
elmasry631 <ibrahimelmasry631@gmail.com>
2025-05-30 21:57:04 +0300

    modified static files and media

--- job/models.py
+++ job/models.py
@@ -1,12 +1,15 @@
 from random import choice
-
+from uuid import uuid4
+from datetime import datetime
 from django.db import models
-
-
 # Create your models here.
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    unique_name = f"{uuid4().hex}.{ext}"
+    today = datetime.today()
+    return "jobs/%s" %(instance.title) +"/"+ today.strftime("%Y/%m/%d") + "/" + unique_name
 class Job(models.Model):
-    JOB_TYPE = (("Full Time", "Full Time"), ("Part Time", "Part Time"))
-
+    JOB_TYPE = (("Full Time", "Full Time"), ("Part Time", "Part Time"), ("Internship", "Internship"), ("Freelance", "Freelance"))
     title = models.CharField(max_length=100)
     job_type = models.CharField(choices=JOB_TYPE)
     description = models.TextField()
@@ -15,6 +18,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_to)
 
     def __str__(self):
         return self.title
